804fc27c41c9f82d11b6717a77081fc64b54ff19
jacob-fain <jacob.fain@ubihere.com>
2023-11-05 12:38:07 -0500

    Added continious recording playback

--- frigate-api-test/main.py
+++ frigate-api-test/main.py
@@ -38,15 +38,46 @@ def testRead():
 
 def testRetrieveRecording():
 
+    cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
+
+
     c1 = FrigateCamera("http://192.168.0.108:5000",
                        "/home/jacob/Ubihere/Frigate/",
                        "cam1")
 
-    success, path = c1.retrieveRecording(1699195704)
+    timestamp = 1699195704
+    while True:
+        success, path = c1.retrieveRecording(timestamp)
+        timestamp += 10
 
-    if success:
+        if success:
+
+            cap = cv2.VideoCapture(path)
+
+            finished = False
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                # Display the frame in a cv2 window
+                cv2.imshow("Display", frame)
+                cv2.waitKey(30)
+                # Break the loop if the user presses 'q'
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            # Release the video capture and close the cv2 window
+
+            cap.release()
+
+
+
         # Plays the recording in vlc
-        os.system("cvlc " + path)
+        # os.system("cvlc " + path)
+
+
+
 
 #-----------------------------------------------------------------------------------------------------------------------
 
